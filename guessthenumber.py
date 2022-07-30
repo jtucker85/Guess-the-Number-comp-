ab91e36e444e91e03576ea16b41a75225955489e
James Tucker <j.tucker07@gmail.com>
2022-07-30 19:37:02 +0100

    First code of Guess the Number (computer)

---
+++ guessthenumber.py
@@ -0,0 +1,17 @@
+import random
+
+
+compnum = random.randint(1,10)
+guessedright = False
+
+while guessedright == False:
+    usernum = input("Guess a number between 1 and 10: ")
+    try: usint = int(usernum)
+    except:
+        print("Please enter a number")
+        continue
+    if usint == compnum:
+        print("Wow! You got it!")
+        guessedright = True
+    else:
+        print("Sorry! Try again")
